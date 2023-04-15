def3a9b2ea8091d2fc17f31b46b5a2a289205074
Mubeen Bankur <mobee33@hotmail.com>
2023-04-15 13:50:36 +0400

    seeded for shoes

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,30 +9,35 @@ Sneaker.destroy_all
 puts "Purging the database"
 
 Sneaker.create([
-  { name: "Nike Air Max 1",
-    description: "The Nike Air Max 1 is a classic and timeless sneaker.",
+  { name: "Puma Smash V2",
+    description: "The Nike Air Jordan 1 is a classic basketball shoe worn by Michael Jordan in 1985, featuring bold colors and iconic design",
     price: 110,
-    image_url: "https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/aa782e70-2c4a-4bcb-96cf-330d782513d6/air-max-1-premium-2-shoes-Zl7x8b.png"
+    image_url: "https://images.unsplash.com/photo-1608231387042-66d1773070a5?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1074&q=80"
   },
-  { name: "Adidas Ultra Boost",
-    description: "The Adidas Ultra Boost is a popular running and lifestyle sneaker.",
+  { name: "Nike Air Jordan 4",
+    description: "The Nike Air Jordan 4 is a classic basketball sneaker that features iconic design elements such as the plastic wing eyelets and Air cushioning.",
     price: 180,
-    image_url: "https://assets.adidas.com/images/w_1880,f_auto,q_auto/654e72734a6a46209f6fac550091d8d6_9366/FY9123_06_standard.jpg"
+    image_url: "https://images.unsplash.com/photo-1606834330324-544ea3bf3015?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=464&q=80"
   },
   { name: "Converse Chuck Taylor All Star",
-    description: "The Converse Chuck Taylor All Star is a classic basketball sneaker",
+    description: "The Converse Chuck Taylor All Star is a classic canvas basketball sneaker with a timeless design and cultural significance.",
     price: 60,
-    image_url: "https://www.converse.com/dw/image/v2/BCZC_PRD/on/demandware.static/-/Sites-cnv-master-catalog/default/dw577e5d95/images/c_08/M9160_C_08X1.jpg?sw=406"
+    image_url: "https://images.unsplash.com/photo-1578269174279-27814d45033f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
   },
   { name: "New Balance 990",
     description: "The New Balance 990 is a popular running sneaker.",
     price: 200,
-    image_url: "https://i8.amplience.net/i/jpl/sz_137280_a?qlt=92"
+    image_url: "https://images.unsplash.com/photo-1597045566677-8cf032ed6634?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1yZWxhdGVkfDN8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=400&q=60"
   },
   { name: "Continental 80 adidas",
     description: "The Continental 80 Adidas sneaker is a retro-style shoe with a sleek and simple design, featuring a split rubber sole and iconic side stripe.",
     price: 200,
     image_url: "https://images.unsplash.com/flagged/photo-1556637640-2c80d3201be8?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80"
+  },
+  { name: "Continental 80 adidas",
+    description: "The Continental 80 Adidas sneaker is a retro-style shoe with a sleek and simple design, featuring a split rubber sole and iconic side stripe.",
+    price: 200,
+    image_url: "https://images.unsplash.com/photo-1579338908476-3a3a1d71a706?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80"
   }
 ])
 
